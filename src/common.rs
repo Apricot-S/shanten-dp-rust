a06fdf928064791da5fa599b1eed5b6abeaccc1a
Apricot-S <mahjong_apricot@outlook.jp>
2026-05-24 08:07:25 +0900

    refactor: Make internal APIs pub(crate)

--- src/common.rs
+++ src/common.rs
@@ -1,8 +1,8 @@
-pub const NUM_TIDS: usize = 34;
-pub const MAX_SHT: u8 = 100;
+pub(crate) const NUM_TIDS: usize = 34;
+pub(crate) const MAX_SHT: u8 = 100;
 
 #[inline]
-pub fn chmin(x: &mut u8, y: u8) {
+pub(crate) fn chmin(x: &mut u8, y: u8) {
     if *x > y {
         *x = y;
     }
